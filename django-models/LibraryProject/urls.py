68d0aa377c7ae0a1e2e0cf8dd02d67f71d547329
muekep <63904395+muekep@users.noreply.github.com>
2025-07-28 22:45:40 +0300

    Update urls.py

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')), # Include your app's URLs
+    # You can also give it a prefix, e.g., path('library/', include('relationship_app.urls')),
+    # If you use a prefix, the URLs would be /library/books/, /library/libraries/1/, etc.
+]
