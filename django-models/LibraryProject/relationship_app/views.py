3fa16dca6f6cbf721a514cb8dd09fb25f4dc9692
muekep <63904395+muekep@users.noreply.github.com>
2025-07-28 22:50:24 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library # Import your models
+from .models import Library, Book # Import your models
 
 # --- Function-based View ---
 def book_list(request):
