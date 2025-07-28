9f1ab6846a78fd31925e967bcd70d80297bfaa9b
muekep <63904395+muekep@users.noreply.github.com>
2025-07-28 22:43:44 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,6 +5,7 @@ class Author(models.Model):
         return self.name
 class Book(models.Model):
     title = models.CharField(max_length=100)
+    publication_year = models.IntegerField(null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     def __str__(self):
         return f"{self.title} by {self.author.name}"
